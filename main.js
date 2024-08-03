fd44c6b71604d25cd9c09526f54e0d1632fa3b49
NickrodBasiri <basirinickrod@gmail.com>
2024-08-03 19:38:24 -0300

    Boilerplate HTML and basic operation functions.

---
+++ main.js
@@ -0,0 +1,37 @@
+let num1;
+let num2;
+let operator;
+
+console.log(operate(5, 9, "*"));
+
+function operate(num1, num2, operator) {
+    if (operator == "+") {
+        return add(num1, num2);
+    }
+    if (operator == "-") {
+        return subtract(num1, num2);
+    }
+    if (operator == "*") {
+        return multiply(num1, num2);
+    }
+    if (operator == "/") {
+        return divide(num1, num2);
+    }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
